e2b2b89ed790774d8b0de0fbd5e91d864a0321d8
Samuli Suomi <3490745+samulisuomi@users.noreply.github.com>
2020-05-06 01:04:47 +0300

    Move snake

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-let field = [
-  ['t',0,0,0,0,0,0,0,0,0],
-  ['b',0,0,0,0,0,0,0,0,0],
-  ['b','b','h',0,0,0,0,0,0,0],
+this.field = [
+  [5,0,0,0,0,0,0,0,0,0],
+  [4,0,0,0,0,0,0,0,0,0],
+  [3,2,1,0,0,0,0,0,0,0],
   [0,0,0,0,0,0,0,0,0,0],
   [0,0,0,0,0,0,0,0,0,0],
   [0,0,0,0,0,0,0,0,0,0],
@@ -11,16 +11,76 @@ let field = [
   [0,0,0,0,0,0,0,0,0,0],
 ]
 
-const draw = () => {
-  const containerElement = document.querySelector('#snake')
+this.width = this.field[0].length
+this.height = this.field.length
 
-  field.forEach(row => {
+this.direction = 'r'
+this.length = 5
+this.headY = 2
+this.headX = 2
+this.tailY = 0
+this.tailX = 0
+this.gameOver = false
+
+window.foo = this
+
+this.nextHeadY = () => {
+  switch(this.direction) {
+    case 'u':
+      return this.headY - 1
+    case 'd':
+      return this.headY + 1
+    default:
+      return this.headY
+  }
+}
+
+this.nextHeadX = () => {
+  switch(this.direction) {
+    case 'l':
+      return this.headX - 1
+    case 'r':
+      return this.headX + 1
+    default:
+      return this.headX
+  }
+}
+
+this.moveSnake = () => {
+  const nextHeadY = this.nextHeadY()
+  const nextHeadX = this.nextHeadX()
+
+  if (nextHeadX < 0 || nextHeadX >= this.width || nextHeadY < 0 || nextHeadY >= this.height) {
+    return this.endGame()
+  }
+
+  this.headY = nextHeadY
+  this.headX = nextHeadX
+
+  this.field[nextHeadY][nextHeadX] = 1
+}
+
+this.onTick = () => {
+  if (!this.gameOver) this.moveSnake()
+  this.draw()
+}
+
+this.endGame = () => {
+  this.gameOver = true
+  alert('Game Over')
+}
+
+this.draw = () => {
+  const containerElement = document.querySelector('#snake')
+  containerElement.textContent = ''
+
+  this.field.forEach(row => {
     const rowElement = document.createElement('div')
     rowElement.className = 'row'
 
     row.forEach(cell => {
       const cellElement = document.createElement('div')
-      cellElement.className = `cell ${cell}`
+      cellElement.className = `cell ${cell ? 'snake' : ''}`
 
       rowElement.appendChild(cellElement)
     })
@@ -30,5 +90,6 @@ const draw = () => {
 }
 
 window.addEventListener('DOMContentLoaded', _event => {
-  draw()
+  this.draw()
+  setInterval(this.onTick, 500)
 })
