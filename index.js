031b87fc614e6562724f614b829ae7a50653e655
Samuli Suomi <3490745+samulisuomi@users.noreply.github.com>
2021-02-11 01:18:34 +0200

    Tileize, magics

--- index.js
+++ index.js
@@ -1,28 +1,23 @@
-window.foo = this
+window.SnakeGame = this
 
 this.initialize = () => {
-  this.field = [
-    [0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0],
-    [0,1,'l','l','l',0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0],
-  ]
-  
-  this.width = this.field[0].length
-  this.height = this.field.length
-  
+  this.height = 11
+  this.width = 20
+
+  this.field = new Array(this.height).fill([])
+  this.field.forEach((_cell, index) => {
+    this.field[index] = new Array(this.width).fill(0)
+  })
+  this.headY = this.height - 1
+  this.headX = Math.floor((this.width - 1) / 2)
+  this.tailY = this.headY
+  this.tailX = 0
+  for (let i = this.tailX; i <= this.headX; i++) {
+    this.field[this.tailY][i] = i === this.headX ? 'l' : 'lr'
+  }
+
   this.inputBuffer = []
   this.direction = 'r'
-  this.headY = 8
-  this.headX = 1
-  this.tailY = 0
-  this.tailX = 0
   this.gameOver = false
 }
 
@@ -64,27 +59,20 @@ this.onKeyDown = event => {
   }
 }
 
-this.addInputToBuffer = direction => {
+this.addInputToBuffer = inputDirection => {
   const previousInput = this.inputBuffer[this.inputBuffer.length - 1] || this.direction
-  switch (direction) {
+
+  switch (inputDirection) {
     case 'u':
-      if (previousInput !== 'u' && previousInput !== 'd') {
-        this.inputBuffer = [...this.inputBuffer, direction]
-      }
-      break
-    case 'r':
-      if (previousInput !== 'r' && previousInput !== 'l') {
-        this.inputBuffer = [...this.inputBuffer, direction]
-      }
-      break
     case 'd':
-      if (previousInput !== 'd' && previousInput !== 'u') {
-        this.inputBuffer = [...this.inputBuffer, direction]
+      if (previousInput !== 'u' && previousInput !== 'd') {
+        this.inputBuffer = [...this.inputBuffer, inputDirection]
       }
       break
     case 'l':
-      if (previousInput !== 'l' && previousInput !== 'r') {
-        this.inputBuffer = [...this.inputBuffer, direction]
+    case 'r':
+      if (previousInput !== 'r' && previousInput !== 'l') {
+        this.inputBuffer = [...this.inputBuffer, inputDirection]
       }
       break
   }
@@ -112,9 +100,24 @@ this.nextHeadX = () => {
   }
 }
 
+
+
+this.getInvertedDirection = direction => {
+  switch(direction) {
+    case 'u':
+      return 'd'
+    case 'r':
+      return 'l'
+    case 'd':
+      return 'u'
+    case 'l':
+      return 'r'
+  }
+}
+
 this.moveSnake = () => {
-  const nextHeadY = this.nextHeadY()
   const nextHeadX = this.nextHeadX()
+  const nextHeadY = this.nextHeadY()
 
   if (nextHeadX < 0 || nextHeadX >= this.width || nextHeadY < 0 || nextHeadY >= this.height) {
     return this.endGame()
@@ -122,10 +125,15 @@ this.moveSnake = () => {
 
   // TODO: Check if the snake would collide
 
-  this.headY = nextHeadY
+  const oldHeadX = this.headX
+  const oldHeadY = this.headY
   this.headX = nextHeadX
+  this.headY = nextHeadY
 
-  this.field[nextHeadY][nextHeadX] = 1
+  this.field[oldHeadY][oldHeadX] = [this.field[oldHeadY][oldHeadX], this.direction].sort().join('')
+  this.field[this.headY][this.headX] = this.getInvertedDirection(this.direction)
+
+
 }
 
 this.onTick = () => {
@@ -135,8 +143,10 @@ this.onTick = () => {
     this.direction = firstInput
     this.inputBuffer = otherInputs
   }
-  if (!this.gameOver) this.moveSnake()
-  this.draw()
+  if (!this.gameOver) {
+    this.moveSnake()
+    this.draw()
+  }
 }
 
 this.endGame = () => {
@@ -144,23 +154,93 @@ this.endGame = () => {
   console.log('Game over')
 }
 
+this.getTileName = directions => {
+  switch(directions) {
+    case 'lr':
+    case 'r':
+    case 'ru':
+      return 'lr-r-ru'
+    case 'd':
+    case 'dl':
+    case 'du':
+      return 'd-dl-du'
+    case 'l':
+    case 'lu':
+    case 'u':
+      return 'l-lu-u'
+    case 'dr':
+      return 'dr'
+  }
+}
+
 this.draw = () => {
   const containerElement = document.querySelector('.snake')
   containerElement.textContent = ''
+
+  // Top border:
+
+  const topBorderRowElement = document.createElement('div')
+  topBorderRowElement.className = 'row'
+
+  const topLeftBorderCellElement = document.createElement('div')
+  topLeftBorderCellElement.className = 'border-cell top-left'
+  topBorderRowElement.appendChild(topLeftBorderCellElement)
+
+  for (let i = 0; i < this.width; i++) {
+    const borderCellElement = document.createElement('div')
+    borderCellElement.className = 'border-cell top'
+    topBorderRowElement.appendChild(borderCellElement)
+  }
+
+  const topRightBorderCellElement = document.createElement('div')
+  topRightBorderCellElement.className = 'border-cell top-right'
+  topBorderRowElement.appendChild(topRightBorderCellElement)
+
+  containerElement.appendChild(topBorderRowElement)
+
+  // Left border + playable area + right border:
 
   this.field.forEach(row => {
     const rowElement = document.createElement('div')
     rowElement.className = 'row'
 
+    const leftBorderCellElement = document.createElement('div')
+    leftBorderCellElement.className = 'border-cell left'
+    rowElement.appendChild(leftBorderCellElement)
+
     row.forEach(cell => {
       const cellElement = document.createElement('div')
-      cellElement.className = `cell ${cell ? 'snake' : ''}`
-
+      cellElement.className = `cell ${cell ? `snake ${this.getTileName(cell)}` : ''}`
       rowElement.appendChild(cellElement)
     })
 
+    const rightBorderCellElement = document.createElement('div')
+    rightBorderCellElement.className = 'border-cell right'
+    rowElement.appendChild(rightBorderCellElement)
+
     containerElement.appendChild(rowElement)
   })
+
+  // Bottom border:
+
+  const bottomBorderRowElement = document.createElement('div')
+  bottomBorderRowElement.className = 'row'
+
+  const bottomLeftBorderCellElement = document.createElement('div')
+  bottomLeftBorderCellElement.className = 'border-cell bottom-left'
+  bottomBorderRowElement.appendChild(bottomLeftBorderCellElement)
+
+  for (let i = 0; i < this.width; i++) {
+    const borderCellElement = document.createElement('div')
+    borderCellElement.className = 'border-cell bottom'
+    bottomBorderRowElement.appendChild(borderCellElement)
+  }
+
+  const bottomRightBorderCellElement = document.createElement('div')
+  bottomRightBorderCellElement.className = 'border-cell bottom-right'
+  bottomBorderRowElement.appendChild(bottomRightBorderCellElement)
+
+  containerElement.appendChild(bottomBorderRowElement)
 }
 
 window.addEventListener('DOMContentLoaded', _event => {
